b151ff1c090609bcee9f2fe890bf24953dbf4bd2
EC2 Default User <ec2-user@ip-10-0-1-132.ec2.internal>
2018-02-16 03:30:41 +0000

    Added timeout

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 echo "Running latest version"
 mkdir s3files
 cd s3files
-wget https://datasets.imdbws.com/name.basics.tsv.gz
-wget https://datasets.imdbws.com/title.akas.tsv.gz
-wget https://datasets.imdbws.com/title.basics.tsv.gz
-wget https://datasets.imdbws.com/title.crew.tsv.gz
-wget https://datasets.imdbws.com/title.episode.tsv.gz
-wget https://datasets.imdbws.com/title.principals.tsv.gz
-wget https://datasets.imdbws.com/title.ratings.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/name.basics.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.akas.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.basics.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.crew.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.episode.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.principals.tsv.gz
+wget --timeout=10 https://datasets.imdbws.com/title.ratings.tsv.gz
 cd ..
 
 cd imdbpy
